2a2f3c037d597a9a2feea39fc33bc2fd2a3a4ecc
rotilho <felipe@rotilho.com>
2024-10-21 09:01:46 +0200

    Emit null in UnsafeSeedDataSource when seed empty

--- composeApp/src/desktopMain/kotlin/cash/atto/wallet/datasource/UnsafeSeedDataSource.kt
+++ composeApp/src/desktopMain/kotlin/cash/atto/wallet/datasource/UnsafeSeedDataSource.kt
@@ -1,7 +1,7 @@
 package cash.atto.wallet.datasource
 
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import java.io.File
 
@@ -13,11 +13,18 @@ class UnsafeSeedDataSource : SeedDataSourceDesktopImpl {
             seedFile.createNewFile()
     }
 
-    override val seed = seedFile.inputStream()
-        .bufferedReader()
-        .lineSequence()
-        .asFlow()
-        .flowOn(Dispatchers.IO)
+    override val seed = flow {
+        if (seedFile.length() == 0L) {
+            emit(null)
+        }
+
+        seedFile.inputStream()
+            .bufferedReader()
+            .lineSequence()
+            .forEach { line ->
+                emit(line)
+            }
+    }.flowOn(Dispatchers.IO)
 
     override suspend fun setSeed(seed: String) = seedFile.writeText(seed)
     override suspend fun clearSeed() {
