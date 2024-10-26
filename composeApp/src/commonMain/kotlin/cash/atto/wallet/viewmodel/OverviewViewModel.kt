291211c8cc18a7ced4003b3546c7ec12129244a7
rotilho <felipe@rotilho.com>
2024-10-26 09:53:47 +0200

    Bump commons

--- composeApp/src/commonMain/kotlin/cash/atto/wallet/viewmodel/OverviewViewModel.kt
+++ composeApp/src/commonMain/kotlin/cash/atto/wallet/viewmodel/OverviewViewModel.kt
@@ -3,34 +3,16 @@ package cash.atto.wallet.viewmodel
 import androidx.lifecycle.ViewModel
 import cash.atto.commons.AttoAddress
 import cash.atto.commons.AttoAlgorithm
-import cash.atto.commons.AttoBlockType
-import cash.atto.commons.AttoNetwork
 import cash.atto.commons.AttoUnit
-import cash.atto.commons.gatekeeper.AttoAuthenticator
-import cash.atto.commons.gatekeeper.attoBackend
 import cash.atto.commons.toAddress
-import cash.atto.commons.toSigner
-import cash.atto.commons.wallet.AttoNodeClient
 import cash.atto.commons.wallet.AttoTransactionRepository
 import cash.atto.commons.wallet.AttoWalletManager
-import cash.atto.commons.wallet.AttoWalletViewer
-import cash.atto.commons.wallet.AttoWorkCache
-import cash.atto.commons.wallet.attoBackend
-import cash.atto.commons.wallet.inMemory
-import cash.atto.commons.work.AttoWorker
-import cash.atto.commons.work.attoBackend
 import cash.atto.wallet.interactor.CreateWalletManagerInteractor
 import cash.atto.wallet.repository.AppStateRepository
-import cash.atto.wallet.state.AppState
-import cash.atto.wallet.uistate.overview.OverviewHeaderUiState
 import cash.atto.wallet.uistate.overview.OverviewUiState
-import cash.atto.wallet.uistate.overview.TransactionListUiState
-import cash.atto.wallet.uistate.overview.TransactionType
-import cash.atto.wallet.uistate.overview.TransactionUiState
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.filterNotNull
@@ -58,11 +40,13 @@ class OverviewViewModel(
 
             appStateRepository.state.collect {
                 walletState.emit(createWalletManagerInteractor.invoke(it))
-                _state.emit(state.value.copy(
-                    receiveAddress = it.publicKey
-                        ?.toAddress(AttoAlgorithm.V1)
-                        ?.value
-                ))
+                _state.emit(
+                    state.value.copy(
+                        receiveAddress = it.publicKey
+                            ?.toAddress(AttoAlgorithm.V1)
+                            ?.value
+                    )
+                )
             }
         }
 
