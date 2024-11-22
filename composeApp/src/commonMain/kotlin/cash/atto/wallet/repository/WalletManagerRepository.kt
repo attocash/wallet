b3c748042af1873f185039859f3b4c66c2beec29
rotilho <felipe@rotilho.com>
2024-11-22 20:42:06 +0100

    Add prod default representatives

--- composeApp/src/commonMain/kotlin/cash/atto/wallet/repository/WalletManagerRepository.kt
+++ composeApp/src/commonMain/kotlin/cash/atto/wallet/repository/WalletManagerRepository.kt
@@ -1,27 +1,35 @@
 package cash.atto.wallet.repository
 
-import cash.atto.commons.AttoAddress
-import cash.atto.commons.AttoAlgorithm
-import cash.atto.commons.AttoNetwork
+import cash.atto.commons.*
 import cash.atto.commons.gatekeeper.AttoAuthenticator
 import cash.atto.commons.gatekeeper.attoBackend
-import cash.atto.commons.toSigner
-import cash.atto.commons.wallet.AttoNodeClient
-import cash.atto.commons.wallet.AttoWalletManager
-import cash.atto.commons.wallet.AttoWalletViewer
-import cash.atto.commons.wallet.AttoWorkCache
-import cash.atto.commons.wallet.attoBackend
-import cash.atto.commons.wallet.inMemory
+import cash.atto.commons.wallet.*
 import cash.atto.commons.worker.AttoWorker
 import cash.atto.commons.worker.attoBackend
 import cash.atto.wallet.state.AppState
-import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
+
+
+private val defaultRepresentatives = listOf(
+    AttoAddress(
+        AttoAlgorithm.V1,
+        AttoPublicKey("6AA1B03B44706F74BD9E4E56A2D4A48E267A22472ABA35EDC06C3B2D81813133".fromHexToByteArray())
+    ),
+    AttoAddress(
+        AttoAlgorithm.V1,
+        AttoPublicKey("89A4A906C0E12AC7CE2F47E76FD4BA01F31F468DE93F4C880F060E346DFE8BD8".fromHexToByteArray())
+    ),
+    AttoAddress(
+        AttoAlgorithm.V1,
+        AttoPublicKey("F398E841D500B9697804D956476120BF235A74A34C4367CC6CDEA53A1268A9BC".fromHexToByteArray())
+    ),
+    AttoAddress(
+        AttoAlgorithm.V1,
+        AttoPublicKey("B3536E6858291B18432AEEF6127134FB6D0C4786A7EFCC97D05427338945E657".fromHexToByteArray())
+    ),
+)
 
 class WalletManagerRepository(
     private val appStateRepository: AppStateRepository,
@@ -74,7 +82,7 @@ class WalletManagerRepository(
             worker = AttoWorker.attoBackend(authenticator),
             workCache = AttoWorkCache.inMemory()
         ) {
-            AttoAddress(AttoAlgorithm.V1, signer.publicKey)
+            defaultRepresentatives.random()
         }
 
         walletManager.start()
