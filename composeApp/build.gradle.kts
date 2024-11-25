5c76753175bcfc0095e865bbecef97e1dddf5d62
rotilho <felipe@rotilho.com>
2024-11-25 09:09:17 +0100

    Bump wallet version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -180,7 +180,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Deb, TargetFormat.Msi, TargetFormat.Dmg)
             packageName = "AttoWallet"
-            packageVersion = "1.0.2"
+            packageVersion = "1.0.3"
             modules("jdk.charsets")
 
             linux {
