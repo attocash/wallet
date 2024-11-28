a8793d08857b433cc3d9c77c734afc10cb5701af
George <ridiculousman@yandex.ru>
2024-11-28 16:55:39 +0400

    Create secret screen redesign on desktop

--- composeApp/src/commonMain/kotlin/cash/atto/wallet/components/common/AttoOnboardingContainer.kt
+++ composeApp/src/commonMain/kotlin/cash/atto/wallet/components/common/AttoOnboardingContainer.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
@@ -19,6 +20,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 fun AttoOnboardingContainer(
     modifier: Modifier = Modifier,
     verticalArrangement: Arrangement.Vertical = Arrangement.spacedBy(12.dp),
+    horizontalAlignment: Alignment.Horizontal = Alignment.Start,
     content: @Composable (ColumnScope) -> Unit
 ) = Column(
     modifier = modifier.clip(MaterialTheme.shapes.medium)
@@ -32,6 +34,7 @@ fun AttoOnboardingContainer(
         )
         .padding(40.dp),
     verticalArrangement = verticalArrangement,
+    horizontalAlignment = horizontalAlignment,
     content = content
 )
 
--- composeApp/src/commonMain/kotlin/cash/atto/wallet/screens/SecretPhraseScreen.kt
+++ composeApp/src/commonMain/kotlin/cash/atto/wallet/screens/SecretPhraseScreen.kt
@@ -1,34 +1,49 @@
 package cash.atto.wallet.screens
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ClipboardManager
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
 import attowallet.composeapp.generated.resources.Res
+import attowallet.composeapp.generated.resources.atto_welcome_background
 import attowallet.composeapp.generated.resources.secret_backup
 import attowallet.composeapp.generated.resources.secret_copy
 import attowallet.composeapp.generated.resources.secret_title
 import cash.atto.wallet.components.common.AppBar
+import cash.atto.wallet.components.common.AttoButton
+import cash.atto.wallet.components.common.AttoOnboardingContainer
 import cash.atto.wallet.components.common.AttoOutlinedButton
 import cash.atto.wallet.components.secret.SecretPhraseGridCompact
+import cash.atto.wallet.components.secret.SecretPhraseGridExpanded
 import cash.atto.wallet.ui.AttoWalletTheme
 import cash.atto.wallet.uistate.secret.SecretPhraseUiState
 import cash.atto.wallet.viewmodel.SecretPhraseViewModel
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.viewmodel.koinViewModel
@@ -58,8 +73,35 @@ fun SecretPhraseScreen(
     )
 }
 
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 @Composable
 fun SecretPhrase(
+    uiState: SecretPhraseUiState,
+    onBackNavigation: () -> Unit,
+    onBackupConfirmClicked: () -> Unit,
+    onCopyClick: () -> Unit
+) {
+    val windowSizeClass = calculateWindowSizeClass()
+
+    if (windowSizeClass.widthSizeClass == WindowWidthSizeClass.Expanded) {
+        SecretPhraseExpanded(
+            uiState = uiState,
+            onBackNavigation = onBackNavigation,
+            onBackupConfirmClicked = onBackupConfirmClicked,
+            onCopyClick = onCopyClick
+        )
+    } else {
+        SecretPhraseCompact(
+            uiState = uiState,
+            onBackNavigation = onBackNavigation,
+            onBackupConfirmClicked = onBackupConfirmClicked,
+            onCopyClick = onCopyClick
+        )
+    }
+}
+
+@Composable
+fun SecretPhraseCompact(
     uiState: SecretPhraseUiState,
     onBackNavigation: () -> Unit,
     onBackupConfirmClicked: () -> Unit,
@@ -105,11 +147,78 @@ fun SecretPhrase(
     )
 }
 
+@Composable
+fun SecretPhraseExpanded(
+    uiState: SecretPhraseUiState,
+    onBackNavigation: () -> Unit,
+    onBackupConfirmClicked: () -> Unit,
+    onCopyClick: () -> Unit
+) {
+    Scaffold(
+        topBar = { AppBar(onBackNavigation) },
+        modifier = Modifier.paint(
+            painter = painterResource(Res.drawable.atto_welcome_background),
+            contentScale = ContentScale.FillBounds
+        ),
+        backgroundColor = Color.Transparent,
+        content = {
+            Box(Modifier.fillMaxSize()) {
+                AttoOnboardingContainer(
+                    modifier = Modifier.align(Alignment.Center)
+                        .fillMaxWidth(0.7f),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = stringResource(Res.string.secret_title),
+                        style = MaterialTheme.typography.h4
+                    )
+
+                    Spacer(Modifier.height(8.dp))
+
+                    SecretPhraseGridExpanded(
+                        words = uiState.words,
+                        hidden = uiState.hidden
+                    )
+
+                    Spacer(Modifier.height(12.dp))
+
+                    AttoButton(
+                        onClick = onBackupConfirmClicked,
+                        modifier = Modifier.width(480.dp)
+                    ) {
+                        Text(text = stringResource(Res.string.secret_backup))
+                    }
+
+                    AttoOutlinedButton(
+                        onClick = onCopyClick,
+                        modifier = Modifier.width(480.dp)
+                    ) {
+                        Text(text = stringResource(Res.string.secret_copy))
+                    }
+                }
+            }
+        }
+    )
+}
+
 @Preview
 @Composable
-fun SecretPhrasePreview() {
+fun SecretPhraseCompactPreview() {
     AttoWalletTheme {
-        SecretPhrase(
+        SecretPhraseCompact(
+            uiState = SecretPhraseUiState.DEFAULT,
+            onBackNavigation = {},
+            onBackupConfirmClicked = {},
+            onCopyClick = {}
+        )
+    }
+}
+
+@Preview
+@Composable
+fun SecretPhraseExpandedPreview() {
+    AttoWalletTheme {
+        SecretPhraseExpanded(
             uiState = SecretPhraseUiState.DEFAULT,
             onBackNavigation = {},
             onBackupConfirmClicked = {},
