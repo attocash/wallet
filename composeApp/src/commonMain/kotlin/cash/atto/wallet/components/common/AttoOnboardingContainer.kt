7fdfe956b0ebc8a64b98319fddcf2f5182559dbb
George <ridiculousman@yandex.ru>
2024-11-28 16:06:35 +0400

    Import secret screen redesign on desktop

---
+++ composeApp/src/commonMain/kotlin/cash/atto/wallet/components/common/AttoOnboardingContainer.kt
@@ -0,0 +1,46 @@
+package cash.atto.wallet.components.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import cash.atto.wallet.ui.AttoWalletTheme
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun AttoOnboardingContainer(
+    modifier: Modifier = Modifier,
+    verticalArrangement: Arrangement.Vertical = Arrangement.spacedBy(12.dp),
+    content: @Composable (ColumnScope) -> Unit
+) = Column(
+    modifier = modifier.clip(MaterialTheme.shapes.medium)
+        .background(color = MaterialTheme.colors.surface)
+        .border(
+            width = 1.dp,
+            color = MaterialTheme.colors
+                .secondaryVariant
+                .copy(alpha = 0.5f),
+            shape = MaterialTheme.shapes.medium
+        )
+        .padding(40.dp),
+    verticalArrangement = verticalArrangement,
+    content = content
+)
+
+@Preview
+@Composable
+fun AttoOnboardingContainerPreview() {
+    AttoWalletTheme {
+        AttoOnboardingContainer {
+            Text("content")
+        }
+    }
+}
--- composeApp/src/commonMain/kotlin/cash/atto/wallet/screens/ImportSecretScreen.kt
+++ composeApp/src/commonMain/kotlin/cash/atto/wallet/screens/ImportSecretScreen.kt
@@ -3,12 +3,15 @@ package cash.atto.wallet.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.FloatingActionButton
@@ -19,26 +22,38 @@ import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Check
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.key.nativeKeyCode
 import androidx.compose.ui.input.key.onPreviewKeyEvent
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import attowallet.composeapp.generated.resources.Res
+import attowallet.composeapp.generated.resources.atto_background_desktop
+import attowallet.composeapp.generated.resources.atto_welcome_background
+import attowallet.composeapp.generated.resources.password_create_next
 import attowallet.composeapp.generated.resources.secret_import_hint
 import attowallet.composeapp.generated.resources.secret_import_title
 import cash.atto.wallet.components.common.AppBar
+import cash.atto.wallet.components.common.AttoButton
+import cash.atto.wallet.components.common.AttoOnboardingContainer
 import cash.atto.wallet.ui.AttoWalletTheme
 import cash.atto.wallet.uistate.secret.ImportSecretUiState
 import cash.atto.wallet.viewmodel.ImportSecretViewModel
 import kotlinx.coroutines.launch
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.viewmodel.koinViewModel
@@ -70,8 +85,35 @@ fun ImportSecretScreen(
     )
 }
 
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 @Composable
 fun ImportSecret(
+    uiState: ImportSecretUiState,
+    onBackNavigation: () -> Unit,
+    onInputChanged: (String) -> Unit,
+    onDoneClicked: () -> Unit
+) {
+    val windowSizeClass = calculateWindowSizeClass()
+
+    if (windowSizeClass.widthSizeClass == WindowWidthSizeClass.Expanded) {
+        ImportSecretExpanded(
+            uiState = uiState,
+            onBackNavigation = onBackNavigation,
+            onInputChanged = onInputChanged,
+            onDoneClicked = onDoneClicked
+        )
+    } else {
+        ImportSecretCompact(
+            uiState = uiState,
+            onBackNavigation = onBackNavigation,
+            onInputChanged = onInputChanged,
+            onDoneClicked = onDoneClicked
+        )
+    }
+}
+
+@Composable
+fun ImportSecretCompact(
     uiState: ImportSecretUiState,
     onBackNavigation: () -> Unit,
     onInputChanged: (String) -> Unit,
@@ -146,11 +188,102 @@ fun ImportSecret(
     )
 }
 
+@Composable
+fun ImportSecretExpanded(
+    uiState: ImportSecretUiState,
+    onBackNavigation: () -> Unit,
+    onInputChanged: (String) -> Unit,
+    onDoneClicked: () -> Unit
+) {
+    Scaffold(
+        topBar = { AppBar(onBackNavigation) },
+        modifier = Modifier.paint(
+            painter = painterResource(Res.drawable.atto_welcome_background),
+            contentScale = ContentScale.FillBounds
+        ),
+        backgroundColor = Color.Transparent,
+        content = {
+            Box(Modifier.fillMaxSize()) {
+                AttoOnboardingContainer(
+                    modifier = Modifier.align(Alignment.Center)
+                        .width(560.dp),
+                    verticalArrangement = Arrangement.Top
+                ) {
+                    Text(
+                        text = stringResource(Res.string.secret_import_title),
+                        style = MaterialTheme.typography.h4
+                    )
+
+                    Text(
+                        text = stringResource(Res.string.secret_import_hint),
+                        modifier = Modifier.padding(top = 25.dp)
+                    )
+
+                    TextField(
+                        value = uiState.input.orEmpty(),
+                        onValueChange = {
+                            onInputChanged.invoke(it)
+                        },
+                        modifier = Modifier.fillMaxWidth()
+                            .padding(top = 12.dp)
+                            .onPreviewKeyEvent {
+                                if (it.key.nativeKeyCode == Key.Enter.nativeKeyCode){
+                                    onDoneClicked.invoke()
+
+                                    return@onPreviewKeyEvent true
+                                }
+
+                                return@onPreviewKeyEvent false
+                            },
+                        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                        keyboardActions = KeyboardActions(
+                            onDone = { onDoneClicked.invoke() }
+                        )
+                    )
+
+                    if (!uiState.inputValid) {
+                        Text(
+                            text = uiState.errorMessage.orEmpty(),
+                            modifier = Modifier.padding(top = 4.dp),
+                            color = MaterialTheme.colors.error,
+                            style = MaterialTheme.typography.caption
+                        )
+                    }
+
+                    AttoButton(
+                        onClick = onDoneClicked,
+                        modifier = Modifier.fillMaxWidth()
+                            .padding(top = 40.dp)
+                    ) {
+                        Text(stringResource(Res.string.password_create_next))
+                    }
+                }
+            }
+        }
+    )
+}
+
 @Preview
 @Composable
-fun ImportSecretPreview() {
+fun ImportSecretCompactPreview() {
     AttoWalletTheme {
-        ImportSecret(
+        ImportSecretCompact(
+            uiState = ImportSecretUiState(
+                input = "ring mask spirit scissors best differ mean pet print century loyal major brain path already version jaguar rescue elder slender anxiety behind leg pigeon",
+                errorMessage = "Input not valid"
+            ),
+            onBackNavigation = {},
+            onInputChanged = {},
+            onDoneClicked = {}
+        )
+    }
+}
+
+@Preview
+@Composable
+fun ImportSecretExpandedPreview() {
+    AttoWalletTheme {
+        ImportSecretExpanded(
             uiState = ImportSecretUiState(
                 input = "ring mask spirit scissors best differ mean pet print century loyal major brain path already version jaguar rescue elder slender anxiety behind leg pigeon",
                 errorMessage = "Input not valid"
