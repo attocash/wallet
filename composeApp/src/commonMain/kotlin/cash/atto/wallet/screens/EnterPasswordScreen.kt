bb2251a6306b136f6e2ef03597cf58404bd3b609
George <ridiculousman@yandex.ru>
2024-11-29 17:56:20 +0400

    Desktop redesign finished

--- composeApp/src/commonMain/kotlin/cash/atto/wallet/screens/EnterPasswordScreen.kt
+++ composeApp/src/commonMain/kotlin/cash/atto/wallet/screens/EnterPasswordScreen.kt
@@ -2,44 +2,69 @@ package cash.atto.wallet.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.key.Key
-import androidx.compose.ui.input.key.key
-import androidx.compose.ui.input.key.nativeKeyCode
-import androidx.compose.ui.input.key.onKeyEvent
-import androidx.compose.ui.input.key.onPreviewKeyEvent
-import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import attowallet.composeapp.generated.resources.Res
+import attowallet.composeapp.generated.resources.atto_welcome_background
 import attowallet.composeapp.generated.resources.password_enter_hint
 import attowallet.composeapp.generated.resources.password_enter_submit
 import attowallet.composeapp.generated.resources.password_enter_title
 import attowallet.composeapp.generated.resources.password_wrong
+import cash.atto.wallet.components.common.AttoButton
+import cash.atto.wallet.components.common.AttoOnboardingContainer
+import cash.atto.wallet.components.common.AttoTextField
 import cash.atto.wallet.ui.AttoWalletTheme
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 @Composable
 fun EnterPassword(
+    onSubmitPassword: (String?) -> Unit,
+    passwordValid: Boolean = true
+) {
+    val windowSizeClass = calculateWindowSizeClass()
+
+    if (windowSizeClass.widthSizeClass == WindowWidthSizeClass.Expanded) {
+        EnterPasswordExpanded(
+            onSubmitPassword = onSubmitPassword,
+            passwordValid = passwordValid
+        )
+    } else {
+        EnterPasswordCompact(
+            onSubmitPassword = onSubmitPassword,
+            passwordValid = passwordValid
+        )
+    }
+}
+
+@Composable
+fun EnterPasswordCompact(
     onSubmitPassword: (String?) -> Unit,
     passwordValid: Boolean = true
 ) {
@@ -64,36 +89,24 @@ fun EnterPassword(
                 style = MaterialTheme.typography.h5
             )
 
-            TextField(
+            AttoTextField(
                 value = input.value.orEmpty(),
                 onValueChange = { input.value = it },
-                modifier = Modifier.onPreviewKeyEvent {
-                    if (it.key.nativeKeyCode == Key.Enter.nativeKeyCode){
-                        onSubmitPassword.invoke(input.value)
-
-                        return@onPreviewKeyEvent true
-                    }
-
-                    return@onPreviewKeyEvent false
-                },
                 placeholder = {
                     Text(text = stringResource(Res.string.password_enter_hint))
                 },
                 visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-                keyboardActions = KeyboardActions(
-                    onDone = { onSubmitPassword.invoke(input.value) }
-                )
+                onDone = { onSubmitPassword.invoke(input.value) },
+                isError = !passwordValid,
+                errorLabel = {
+                    Text(
+                        text = stringResource(Res.string.password_wrong),
+                        color = MaterialTheme.colors.error,
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.caption
+                    )
+                }
             )
-
-            if (!passwordValid) {
-                Text(
-                    text = stringResource(Res.string.password_wrong),
-                    color = MaterialTheme.colors.error,
-                    textAlign = TextAlign.Center,
-                    style = MaterialTheme.typography.caption
-                )
-            }
 
             Spacer(Modifier.weight(1f))
 
@@ -105,14 +118,81 @@ fun EnterPassword(
             }
         }
     }
+}
 
+@Composable
+fun EnterPasswordExpanded(
+    onSubmitPassword: (String?) -> Unit,
+    passwordValid: Boolean = true
+) {
+    val input = remember {
+        mutableStateOf<String?>(null)
+    }
+
+    Box(Modifier.fillMaxSize()
+        .paint(
+            painter = painterResource(Res.drawable.atto_welcome_background),
+            contentScale = ContentScale.FillBounds
+        )
+    ) {
+        AttoOnboardingContainer(
+            modifier = Modifier.align(Alignment.Center)
+                .width(560.dp)
+        ) {
+            Text(
+                text = stringResource(Res.string.password_enter_title),
+                style = MaterialTheme.typography.h4
+            )
+
+            Spacer(Modifier.height(1.dp))
+
+            AttoTextField(
+                value = input.value.orEmpty(),
+                onValueChange = { input.value = it },
+                modifier = Modifier.fillMaxWidth(),
+                placeholder = {
+                    Text(text = stringResource(Res.string.password_enter_hint))
+                },
+                visualTransformation = PasswordVisualTransformation(),
+                onDone = { onSubmitPassword.invoke(input.value) },
+                isError = !passwordValid,
+                errorLabel = {
+                    Text(
+                        text = stringResource(Res.string.password_wrong),
+                        color = MaterialTheme.colors.error,
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.caption
+                    )
+                }
+            )
+
+            Spacer(Modifier.height(1.dp))
+
+            AttoButton(
+                onClick = { onSubmitPassword.invoke(input.value) },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = stringResource(Res.string.password_enter_submit))
+            }
+        }
+    }
 }
 
 @Preview
 @Composable
-fun EnterPasswordPreview() {
+fun EnterPasswordCompactPreview() {
     AttoWalletTheme {
-        EnterPassword(
+        EnterPasswordCompact(
+            onSubmitPassword = {}
+        )
+    }
+}
+
+@Preview
+@Composable
+fun EnterPasswordExpandedPreview() {
+    AttoWalletTheme {
+        EnterPasswordExpanded(
             onSubmitPassword = {}
         )
     }
