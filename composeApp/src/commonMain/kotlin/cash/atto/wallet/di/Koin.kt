a25972908d04de598c6f1f957a1238e8207949ea
George <ridiculousman@yandex.ru>
2025-01-14 20:07:54 +0400

    web target now does something

--- composeApp/src/commonMain/kotlin/cash/atto/wallet/di/Koin.kt
+++ composeApp/src/commonMain/kotlin/cash/atto/wallet/di/Koin.kt
@@ -1,6 +1,6 @@
 package cash.atto.wallet.di
 
-import org.koin.core.context.GlobalContext.startKoin
+import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
 
 fun initKoin(appDeclaration: KoinAppDeclaration = {}) = startKoin {
