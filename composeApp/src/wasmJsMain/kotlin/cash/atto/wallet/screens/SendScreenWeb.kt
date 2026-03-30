d522eaf5c06490cd5da0a65bb4e98dfc9a47c6c3
rotilho <felipe@rotilho.com>
2026-03-30 22:26:51 +0200

    Allow scroll in send screen

--- composeApp/src/wasmJsMain/kotlin/cash/atto/wallet/screens/SendScreenWeb.kt
+++ composeApp/src/wasmJsMain/kotlin/cash/atto/wallet/screens/SendScreenWeb.kt
@@ -9,9 +9,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -180,6 +182,7 @@ fun SendFromWeb(
     Column(
         modifier = Modifier.clip(RoundedCornerShape(50.dp))
             .background(color = MaterialTheme.colorScheme.surface)
+            .verticalScroll(rememberScrollState())
             .padding(vertical = 48.dp, horizontal = 16.dp),
         verticalArrangement = Arrangement.spacedBy(12.dp),
         horizontalAlignment = Alignment.CenterHorizontally
