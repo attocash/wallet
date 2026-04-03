50ef8087d902a2222ec1037b8f52bb63c1219b36
rotilho <felipe@rotilho.com>
2026-04-03 23:05:40 +0200

    Add PendingReceivablesState

---
+++ composeApp/src/commonMain/kotlin/cash/atto/wallet/repository/PendingReceivablesState.kt
@@ -0,0 +1,23 @@
+package cash.atto.wallet.repository
+
+import cash.atto.commons.AttoReceivable
+import cash.atto.commons.AttoUnit
+import com.ionspin.kotlin.bignum.decimal.BigDecimal
+import com.ionspin.kotlin.bignum.decimal.toBigDecimal
+
+data class PendingReceivablesState(
+    val receivables: List<AttoReceivable> = emptyList()
+) {
+    val count: Int
+        get() = receivables.size
+
+    val totalAmount: BigDecimal
+        get() = receivables
+            .fold(BigDecimal.ZERO) { acc, receivable ->
+                acc + receivable.amount.toString(AttoUnit.ATTO).toBigDecimal()
+            }
+
+    companion object {
+        val EMPTY = PendingReceivablesState()
+    }
+}
