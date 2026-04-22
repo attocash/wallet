1d8a3592cd4fa638b3548f5554d03c8b6584f5f4
rotilho <felipe@rotilho.com>
2026-04-22 20:19:13 +0200

    Fix puppeteer.js

--- composeApp/karma.config.d/puppeteer.js
+++ composeApp/karma.config.d/puppeteer.js
@@ -1,8 +1,1 @@
-const path = require('path');
-
-// Point Puppeteer cache to project-local directory
-// When inlined into karma.conf.js, __dirname is build/wasm/packages/attoWallet-test/
-// so we need four levels up to reach the project root
-process.env.PUPPETEER_CACHE_DIR = path.join(__dirname, '..', '..', '..', '..', '.cache', 'puppeteer');
-
 process.env.CHROME_BIN = require('puppeteer').executablePath();
